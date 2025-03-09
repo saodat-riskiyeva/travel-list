80565a5c83fabe0cc8a984609f1c89f7603eb00e
Saodat <saodat.riskiyeva@gmail.com>
2025-03-09 00:54:20 -0500

    working at aorting feature

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ export default function App() {
   function handleClearList(items) {
     setItems([]);
   }
+
   return (
     <div className="app">
       <Logo />
@@ -91,10 +92,23 @@ function Form({ onAddItems }) {
 }
 
 function PakingList({ items, onDeleteItem, onToggleItem, onClearList }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -104,7 +118,21 @@ function PakingList({ items, onDeleteItem, onToggleItem, onClearList }) {
         ))}
       </ul>
       <div className="actions">
-        <button> Sort </button>
+        <select
+          className="dropDown"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="input" key="1">
+            Sort by input order
+          </option>
+          <option value="description" key="2">
+            Sort by description
+          </option>
+          <option value="packed" key="3">
+            Sort by packed status
+          </option>
+        </select>
         <button onClick={() => onClearList()}> Clear list </button>
       </div>
     </div>
